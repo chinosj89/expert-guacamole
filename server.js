51e60cf4f45fcac5a48d8260a2017fe3502c5493
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-09-21 16:11:06 -0700

    trying fix

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -15,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Raphael Super secret secret',
+    secret: process.env.SESSION_SECRET,
     cookie: {
         maxAge: 300000,
         httpOnly: true,
